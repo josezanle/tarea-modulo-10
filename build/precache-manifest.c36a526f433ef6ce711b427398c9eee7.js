e088b9bfe38c5d6814ed6baa177b1afd13dab7b8
josezanle <63666176+josezanle@users.noreply.github.com>
2020-09-09 20:33:22 -0300

    carpeta build

---
+++ build/precache-manifest.c36a526f433ef6ce711b427398c9eee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f0f068b53aaf326e2529038b2d906b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15f65261ac346e5305b0",
+    "url": "/static/css/main.4991e222.chunk.css"
+  },
+  {
+    "revision": "b1308903c189786f008a",
+    "url": "/static/js/2.0931ed5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0931ed5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f65261ac346e5305b0",
+    "url": "/static/js/main.4c0468ba.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
